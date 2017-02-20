36125e3630a4df98953f8473e46d27758495cfe9
Martin Hoffmann <hn@nvnc.de>
2017-02-20 14:00:58 +0100

    Add an alt! macro.

--- src/macros.rs
+++ src/macros.rs
@@ -1,14 +1,35 @@
 
 /// A macro for extracting the result from a `Poll<T, E>`.
 ///
-/// Turns the `Poll<T, E>` into a `Result<T, F>` by early returning a
-/// possible `Ok(Async::NotReady)` and calling `F::from()` for an error.
+/// Turns the `Poll<T, E>` into a `Result<T, E>` by early returning a
+/// possible `Ok(Async::NotReady)`.
 #[macro_export]
 macro_rules! try_result {
     ($e:expr) => (match $e {
         Ok($crate::Async::Ready(t)) => Ok(t),
         Ok($crate::Async::NotReady) => return Ok($crate::Async::NotReady),
-        Err(e) => Err(From::from(e)),
+        Err(e) => Err(e),
     })
 }
 
+#[macro_export]
+macro_rules! alt {
+    ( $e:expr, $( $tail:tt )* ) => {{
+        match $e {
+            Ok($crate::Async::Ready(t)) => Ok($crate::Async::Ready(t)),
+            Ok($crate::Async::NotReady) => Ok($crate::Async::NotReady),
+            Err(_) => {
+                alt!( $( $tail )* )
+            }
+        }
+    }};
+
+    ( $e:expr ) => {{
+        match $e {
+            Ok($crate::Async::Ready(t)) => Ok($crate::Async::Ready(t)),
+            Ok($crate::Async::NotReady) => Ok($crate::Async::NotReady),
+            Err(err) => Err(err),
+        }
+    }};
+
+}
